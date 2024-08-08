c3e2539584437486ac08d3bff8033341a7fd799e
rjhallsted <rjhallsted@gmail.com>
2024-08-08 14:28:57 -0600

    Lots of progress on switching to serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,224 +1,34 @@
-use core::str;
-use std::error::Error;
+use serde::{self, Deserialize, Serialize};
 
 pub mod generate;
+pub mod storage;
 
-enum PersistedDataParsingError {
-    ConversionErr(Box<dyn Error>),
-    DBTypeError,
-}
-
-struct ParseResult<T> {
-    res: T,
-    bytes_read: usize,
-}
-
-trait Persistable {
-    fn to_persistent_bytes(&self) -> Vec<u8>;
-    // TODO: Try to do this withouth needing to box the original error
-    fn from_peristent_bytes(bytes: &[u8]) -> Result<ParseResult<Self>, PersistedDataParsingError>
-    where
-        Self: Sized;
-}
-
+#[derive(Serialize, Deserialize)]
 enum DbType {
     String(String),
     Integer(i32),
     Float(f32),
 }
-impl DbType {
-    fn persistent_int(&self) -> u8 {
-        match self {
-            Self::String(_) => 1,
-            Self::Integer(_) => 2,
-            Self::Float(_) => 3,
-        }
-    }
-}
-impl Persistable for DbType {
-    fn to_persistent_bytes(&self) -> Vec<u8> {
-        // TODO: Make this do fewer passes over the output bytes
-        let type_bytes = Vec::from(self.persistent_int().to_ne_bytes());
-        let val_bytes = match self {
-            Self::String(str) => str.to_persistent_bytes(),
-            Self::Integer(i) => i.to_persistent_bytes(),
-            Self::Float(f) => f.to_persistent_bytes(),
-        };
-        [type_bytes, val_bytes].concat()
-    }
 
-    fn from_peristent_bytes(
-        bytes: &[u8],
-    ) -> Result<ParseResult<DbType>, PersistedDataParsingError> {
-        let type_int = bytes[0];
-        match type_int {
-            1 => {
-                let s = String::from_peristent_bytes(&bytes[1..])?;
-                Ok(ParseResult {
-                    res: DbType::String(s.res),
-                    bytes_read: s.bytes_read + 1,
-                })
-            }
-            2 => {
-                let i = i32::from_peristent_bytes(&bytes[1..])?;
-                Ok(ParseResult {
-                    res: DbType::Integer(i.res),
-                    bytes_read: i.bytes_read + 1,
-                })
-            }
-            3 => {
-                let f = f32::from_peristent_bytes(&bytes[1..])?;
-                Ok(ParseResult {
-                    res: DbType::Float(f.res),
-                    bytes_read: f.bytes_read + 1,
-                })
-            }
-            _ => return Err(PersistedDataParsingError::DBTypeError),
-        }
-    }
-}
-
-impl Persistable for String {
-    fn to_persistent_bytes(&self) -> Vec<u8> {
-        let len_bytes = Vec::from(self.len().to_ne_bytes());
-        let val_bytes = Vec::from(self.as_bytes());
-        [len_bytes, val_bytes].concat()
-    }
-
-    fn from_peristent_bytes(
-        bytes: &[u8],
-    ) -> Result<ParseResult<String>, PersistedDataParsingError> {
-        let len_byte_array: [u8; 8] = bytes[0..8]
-            .try_into()
-            .map_err(|err| PersistedDataParsingError::ConversionErr(Box::new(err)))?;
-        let len = usize::from_ne_bytes(len_byte_array);
-
-        let str_bytes = Vec::from(&bytes[8..8 + len]);
-        assert_eq!(str_bytes.len(), len);
-        let s = String::from_utf8(str_bytes)
-            .map_err(|err| PersistedDataParsingError::ConversionErr(Box::new(err)))?;
-        Ok(ParseResult {
-            res: s,
-            bytes_read: len + 8,
-        })
-    }
-}
-
-impl Persistable for i32 {
-    fn to_persistent_bytes(&self) -> Vec<u8> {
-        Vec::from(self.to_ne_bytes())
-    }
-
-    fn from_peristent_bytes(bytes: &[u8]) -> Result<ParseResult<i32>, PersistedDataParsingError> {
-        let val_bytes = bytes[0..4]
-            .try_into()
-            .map_err(|err| PersistedDataParsingError::ConversionErr(Box::new(err)))?;
-        Ok(ParseResult {
-            res: i32::from_ne_bytes(val_bytes),
-            bytes_read: 4,
-        })
-    }
-}
-
-impl Persistable for f32 {
-    fn to_persistent_bytes(&self) -> Vec<u8> {
-        Vec::from(self.to_ne_bytes())
-    }
-
-    fn from_peristent_bytes(bytes: &[u8]) -> Result<ParseResult<f32>, PersistedDataParsingError> {
-        let val_bytes = bytes[0..4]
-            .try_into()
-            .map_err(|err| PersistedDataParsingError::ConversionErr(Box::new(err)))?;
-        Ok(ParseResult {
-            res: f32::from_ne_bytes(val_bytes),
-            bytes_read: 4,
-        })
-    }
-}
-
-/// # Table Layout Notes
-///
-/// These are layed out in this order on disk
-///
-/// ## DB Header
-/// - header_version: u16
-/// - header_size: u32
-///
-/// ## Metadata Table
-/// ### Table header
-/// - header_version: u16
-/// - header_size: u32
-/// - row_header_version: u16
-/// - row_header_size: u32
-/// - table_name_len: u32
-/// - table_name: str
-/// ### Rows (per-row)
-/// #### Row Header
-/// - row_size: u64
-/// - row_id: u64
-/// #### Row contents (in row-schema)
-///
-///
-
-pub mod storage {
-    use std::{
-        fs,
-        io::{Error, Write},
-        path::Path,
-    };
-
-    use crate::{DbType, Persistable};
-
-    // fn init_db(db_file: &Path) -> Result<(), Error> {
-    //     let file = fs::OpenOptions::new()
-    //         .read(true)
-    //         .write(true)
-    //         .truncate(true)
-    //         .open(db_file)?;
-
-    //     // look for _metadata table. Create if does not exist.
-
-    //     Ok(())
-    // }
-
-    // fn create_table(name: &str, schema: Vec<DbType>) -> Result {
-
-    // }
-
-    struct Row {
-        vals: Vec<DbType>,
-    }
-    impl Persistable for Row {
-        fn to_persistent_bytes(&self) -> Vec<u8> {
-            let mut buffer = Vec::new();
-
-            for v in self.vals.iter() {
-                let mut res = v.to_persistent_bytes();
-                buffer.append(&mut res);
-            }
-
-            buffer
-        }
-
-        fn from_peristent_bytes(
-            bytes: &[u8],
-        ) -> Result<ParseResult<Row>, PersistedDataParsingError> {
-            // TODO: WIP
-        }
-    }
-
-    fn write_to_table(db_file: &Path, rows: Vec<Row>) -> Result<(), Error> {
-        let mut file = fs::OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .open(db_file)?;
-
-        for row in rows {
-            file.write(row.to_persistent_bytes().as_ref())?;
-        }
-
-        file.flush()?;
-
-        Ok(())
-    }
-}
+// / # Table Layout Notes
+// /
+// / These are layed out in this order on disk
+// /
+// / ## DB Header
+// / - header_version: u16
+// / - header_size: u32
+// /
+// / ## Metadata Table
+// / ### Table header
+// / - header_version: u16
+// / - header_size: u32
+// / - row_header_version: u16
+// / - row_header_size: u32
+// / - table_name_len: u32
+// / - table_name: str
+// / ### Rows (per-row)
+// / #### Row Header
+// / - row_size: u64
+// / - row_id: u64
+// / #### Row contents (in row-schema)
+// /
---
+++ src/storage.rs
@@ -0,0 +1,424 @@
+use std::{
+    fmt::Display,
+    fs,
+    io::{self, Error, Write},
+    path::Path,
+};
+
+use serde::{
+    de,
+    ser::{self, SerializeSeq},
+    Deserialize, Serialize,
+};
+
+use crate::DbType;
+
+#[derive(Debug)]
+enum SerdeError {
+    Message(String),
+    WritingError(io::Error),
+}
+impl std::error::Error for SerdeError {}
+impl ser::Error for SerdeError {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: std::fmt::Display,
+    {
+        Self::Message(msg.to_string())
+    }
+}
+impl de::Error for SerdeError {
+    fn custom<T>(msg: T) -> Self
+    where
+        T: std::fmt::Display,
+    {
+        Self::Message(msg.to_string())
+    }
+}
+impl Display for SerdeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Message(msg) => f.write_str(msg),
+            Self::WritingError(err) => err.fmt(f),
+        }
+    }
+}
+impl From<io::Error> for SerdeError {
+    fn from(value: io::Error) -> Self {
+        Self::WritingError(value)
+    }
+}
+
+struct Serializer<'w, T: io::Write> {
+    writer: &'w mut T,
+}
+impl<'w, T: io::Write> Serializer<'w, T> {
+    fn build(writer: &'w mut T) -> Self {
+        Serializer { writer }
+    }
+}
+impl<'a, 'w, T: io::Write> ser::Serializer for &'a mut Serializer<'w, T> {
+    type Ok = ();
+    type Error = SerdeError;
+    type SerializeMap = Self;
+    type SerializeSeq = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+
+    fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
+        let output = if v { 1 } else { 0 };
+        self.writer.write([output].as_slice())?;
+        Ok(())
+    }
+
+    fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
+        let mut seq = self.serialize_seq(Some(v.len()))?;
+        for b in v {
+            seq.serialize_element(v)?;
+        }
+        seq.end()
+    }
+
+    fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
+        self.serialize_str(v.to_string().as_ref())
+    }
+
+    fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_i128(self, v: i128) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_u128(self, v: u128) -> Result<Self::Ok, Self::Error> {
+        let bytes = v.to_le_bytes();
+        self.writer.write(&bytes[..])?;
+        Ok(())
+    }
+
+    fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
+        self.serialize_bytes(v.as_bytes())
+    }
+
+    fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
+        self.serialize_unit()
+    }
+
+    fn serialize_some<U>(self, value: &U) -> Result<Self::Ok, Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        value.serialize(self)
+    }
+
+    fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
+        self.serialize_u8(0)
+    }
+
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<Self::Ok, Self::Error> {
+        self.serialize_unit()
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+    ) -> Result<Self::Ok, Self::Error> {
+        self.serialize_u32(variant_index)
+    }
+
+    fn serialize_newtype_struct<U>(
+        self,
+        _name: &'static str,
+        value: &U,
+    ) -> Result<Self::Ok, Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        value.serialize(self)
+    }
+
+    fn serialize_newtype_variant<U>(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+        value: &U,
+    ) -> Result<Self::Ok, Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        variant_index.serialize(&mut *self)?;
+        value.serialize(&mut *self)
+    }
+
+    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
+        let len = match len {
+            Some(l) => l as u64,
+            None => 0,
+        };
+        self.serialize_u64(len)?;
+        Ok(self)
+    }
+
+    fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeTupleStruct, Self::Error> {
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeTupleVariant, Self::Error> {
+        self.serialize_u32(variant_index);
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
+        self.serialize_seq(len)
+    }
+
+    fn serialize_struct(
+        self,
+        _name: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeStruct, Self::Error> {
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeStructVariant, Self::Error> {
+        self.serialize_u32(variant_index)?;
+        self.serialize_seq(Some(len))
+    }
+}
+impl<'a, 'w, T: io::Write> ser::SerializeSeq for &'a mut Serializer<'w, T> {
+    type Ok = ();
+    type Error = SerdeError;
+
+    fn serialize_element<U>(&mut self, value: &U) -> Result<(), Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+impl<'a, 'w, T: io::Write> ser::SerializeTuple for &'a mut Serializer<'w, T> {
+    type Ok = ();
+    type Error = SerdeError;
+
+    fn serialize_element<U>(&mut self, value: &U) -> Result<(), Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+impl<'a, 'w, T: io::Write> ser::SerializeTupleStruct for &'a mut Serializer<'w, T> {
+    type Ok = ();
+    type Error = SerdeError;
+
+    fn serialize_field<U>(&mut self, value: &U) -> Result<(), Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+impl<'a, 'w, T: io::Write> ser::SerializeTupleVariant for &'a mut Serializer<'w, T> {
+    type Ok = ();
+    type Error = SerdeError;
+
+    fn serialize_field<U>(&mut self, value: &U) -> Result<(), Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+impl<'a, 'w, T: io::Write> ser::SerializeMap for &'a mut Serializer<'w, T> {
+    type Ok = ();
+    type Error = SerdeError;
+
+    fn serialize_key<U>(&mut self, key: &U) -> Result<(), Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        key.serialize(&mut **self)
+    }
+
+    fn serialize_value<U>(&mut self, value: &U) -> Result<(), Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+impl<'a, 'w, T: io::Write> ser::SerializeStruct for &'a mut Serializer<'w, T> {
+    type Ok = ();
+    type Error = SerdeError;
+
+    fn serialize_field<U>(&mut self, _key: &'static str, value: &U) -> Result<(), Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+impl<'a, 'w, T: io::Write> ser::SerializeStructVariant for &'a mut Serializer<'w, T> {
+    type Ok = ();
+    type Error = SerdeError;
+
+    fn serialize_field<U>(&mut self, _key: &'static str, value: &U) -> Result<(), Self::Error>
+    where
+        U: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(())
+    }
+}
+
+fn to_writer<'w, W, T>(writer: &'w mut W, value: &T) -> Result<(), SerdeError>
+where
+    W: io::Write,
+    T: Serialize,
+{
+    let mut serializer = Serializer::build(writer);
+    value.serialize(&mut serializer)
+}
+
+// fn init_db(db_file: &Path) -> Result<(), Error> {
+//     let file = fs::OpenOptions::new()
+//         .read(true)
+//         .write(true)
+//         .truncate(true)
+//         .open(db_file)?;
+
+//     // look for _metadata table. Create if does not exist.
+
+//     Ok(())
+// }
+
+// fn create_table(name: &str, schema: Vec<DbType>) -> Result {
+
+// }
+
+#[derive(Serialize, Deserialize)]
+struct Row {
+    vals: Vec<DbType>,
+}
+
+fn write_to_table(db_file: &Path, rows: Vec<Row>) -> Result<(), SerdeError> {
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(db_file)?;
+    to_writer(&mut file, &rows)?;
+    file.flush()?;
+    Ok(())
+}
