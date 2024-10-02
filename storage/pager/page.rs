256ef2f435bb5a5c8e5a58bc9247f7282e920126
rjhallsted <rjhallsted@gmail.com>
2024-10-02 16:04:51 -0700

    Get disk writes/reads working, including fixing undefined behavior

--- storage/pager/page.rs
+++ storage/pager/page.rs
@@ -2,8 +2,12 @@ use core::slice;
 use std::{
     io::{Error as IoError, Write},
     mem,
+    num::NonZeroU64,
     ops::Range,
+    os::fd::AsFd,
 };
+
+use rustix::io::{pread, pwrite, retry_on_intr, Errno as RustixErrno};
 
 use crate::serialize::{Deserialize, SerdeError, Serialize};
 
@@ -55,9 +59,9 @@ use crate::serialize::{Deserialize, SerdeError, Serialize};
 pub type PageId = u64;
 type PageBufferOffset = u16;
 
-const PAGE_DATA_SIZE: PageBufferOffset = 4096 * 4; // 16KB
+const PAGE_SIZE: PageBufferOffset = 4096 * 4; // 16KB
 const PAGE_BUFFER_SIZE: PageBufferOffset =
-    PAGE_DATA_SIZE - mem::size_of::<PageHeader>() as PageBufferOffset;
+    PAGE_SIZE - mem::size_of::<PageHeader>() as PageBufferOffset;
 const HEADER_VERSION: u8 = 1;
 // the byte values spell PAGE
 const ALIGNMENT_GUARD_VALUE: u32 = u32::from_be_bytes([50, 41, 47, 45]);
@@ -68,6 +72,8 @@ pub enum PageError {
     IoError(IoError),
     SerdeError(SerdeError),
     NotEnoughSpace,
+    RustixError(RustixErrno),
+    Corrupted,
 }
 impl From<IoError> for PageError {
     fn from(value: IoError) -> Self {
@@ -79,8 +85,14 @@ impl From<SerdeError> for PageError {
         Self::SerdeError(value)
     }
 }
+impl From<RustixErrno> for PageError {
+    fn from(value: RustixErrno) -> Self {
+        Self::RustixError(value)
+    }
+}
 
 #[repr(C)]
+#[derive(Debug, PartialEq)]
 struct PageFlags {
     flags: u8,
 }
@@ -112,6 +124,7 @@ impl PageFlags {
 }
 
 #[repr(u8)]
+#[derive(Debug, PartialEq)]
 pub enum PageKind {
     Data,
 }
@@ -122,6 +135,11 @@ pub enum PageKind {
 ///     All changes will requiring incrementing the header_version and maintaining the old & new
 ///     page layouts while pages with the old layout still exist.
 ///
+///     We must also be careful to lay this out in a way that doesn't have any unitialized memory.
+///     This is why we use Option<NonZeroU64> instead of Option<u64> for overflow_page_id. Option<NonZeroU64>
+///     can use a compiler optimization to store the None state as 0, so the enum tag doesn't need
+///     to be stored seperately, meaning we don't need the padding that would require.
+///
 /// checksum and header_version must be at the beginning of every version of this struct so that:
 /// - The checksum can be validated before continuing
 /// - the header_version can be read so that we know what struct version to use
@@ -129,22 +147,26 @@ pub enum PageKind {
 /// Our max page size will be 16KB, so free_space_start and free_space_end only need to be u16. If
 /// the page size increases, those fields will need to use larger types
 #[repr(C)]
+#[derive(Debug, PartialEq)]
 struct PageHeader {
     // comments: size, end-of-this-field-with-padding in layout
-    checksum: u64,                      // 8, 8
-    header_version: u8,                 // 1, 9
-    flags: PageFlags,                   // 1, 10
-    page_kind: PageKind,                // 1, 12
-    alignment_guard: u32,               // 4, 16
-    page_id: PageId,                    // 8, 24
-    overflow_page_id: Option<PageId>,   // 16, 40
-    cell_count: u16,                    // 2, 42
-    free_space_start: PageBufferOffset, // 2, 44
-    free_space_end: PageBufferOffset,   // 2, 46
-    total_free_space: PageBufferOffset, // 2, 48
+    checksum: u64,      // 8, 8
+    header_version: u8, // 1, 9
+    flags: PageFlags,   // 1, 10
+    //  (included so that this memory is initialized instead of being padding)
+    _padding1: u8,                        // 1, 11
+    page_kind: PageKind,                  // 1, 12
+    alignment_guard: u32,                 // 4, 16
+    page_id: PageId,                      // 8, 24
+    overflow_page_id: Option<NonZeroU64>, // 8, 32
+    cell_count: u16,                      // 2, 34
+    free_space_start: PageBufferOffset,   // 2, 36
+    free_space_end: PageBufferOffset,     // 2, 38
+    total_free_space: PageBufferOffset,   // 2, 40
 }
 
 #[repr(C)]
+#[derive(Debug, PartialEq)]
 struct PageBuffer {
     data: [u8; PAGE_BUFFER_SIZE as usize],
 }
@@ -157,12 +179,12 @@ impl PageBuffer {
 
     fn get_writer(&mut self, offset: PageBufferOffset, data_size: usize) -> impl Write + '_ {
         let offset = offset as usize;
-        let size = data_size as usize;
-        &mut self.data[offset..offset + size]
+        &mut self.data[offset..offset + data_size]
     }
 }
 
 #[repr(C)]
+#[derive(Debug, PartialEq)]
 pub struct Page {
     header: PageHeader,
     data: PageBuffer,
@@ -175,6 +197,7 @@ impl Page {
             flags: PageFlags { flags: 0 },
             cell_count: 0,
             page_kind: kind,
+            _padding1: 0,
             alignment_guard: ALIGNMENT_GUARD_VALUE,
             free_space_start: 0,
             free_space_end: PAGE_BUFFER_SIZE,
@@ -187,26 +210,78 @@ impl Page {
             data: [0; PAGE_BUFFER_SIZE as usize],
         };
 
-        let mut page = Page { header, data };
-        page.header.checksum = page.calc_checksum();
-        page
+        Page { header, data }
     }
 
     fn calc_checksum(&self) -> u64 {
-        unsafe {
-            let bytes =
-                slice::from_raw_parts((self as *const Page) as *const u8, mem::size_of::<Page>());
-            checksum(&bytes[8..]).unwrap()
+        let bytes = self.as_slice();
+        checksum(&bytes[8..]).unwrap()
+    }
+
+    pub fn from_disk<Fd: AsFd>(fd: Fd, page_id: PageId) -> Result<Self, PageError> {
+        let mut new_page = Page::new(0, PageKind::Data);
+        let buf = new_page.as_slice_mut();
+        let offset = page_id * PAGE_SIZE as u64;
+        // make read all
+        Page::read_entire_page(fd, buf, offset)?;
+
+        // new page should now have values from disk
+        let checksum = new_page.calc_checksum();
+        if checksum != new_page.header.checksum {
+            return Err(PageError::Corrupted);
         }
+        Ok(new_page)
     }
 
-    // TODO: Fix the error type
-    pub fn from_disk(offset: usize) -> Result<Self, ()> {
-        unimplemented!();
+    fn as_slice(&self) -> &[u8] {
+        unsafe { slice::from_raw_parts(self as *const Self as *const u8, PAGE_SIZE.into()) }
     }
 
-    pub fn flush(&mut self) {
-        unimplemented!();
+    fn as_slice_mut(&mut self) -> &mut [u8] {
+        unsafe { slice::from_raw_parts_mut(self as *mut Self as *mut u8, PAGE_SIZE.into()) }
+    }
+
+    fn read_entire_page<Fd: AsFd>(fd: Fd, buf: &mut [u8], offset: u64) -> Result<(), RustixErrno> {
+        assert!(buf.len() == PAGE_SIZE as usize);
+        let mut buf = buf;
+        let mut offset = offset;
+        let mut bytes_left = PAGE_SIZE as usize;
+        while bytes_left > 0 {
+            let bytes_read = retry_on_intr(|| pread(fd.as_fd(), buf, offset))?;
+            bytes_left -= bytes_read;
+            buf = &mut buf[bytes_read..];
+            offset += bytes_read as u64;
+        }
+        Ok(())
+    }
+
+    fn write_entire_page<Fd: AsFd>(fd: Fd, buf: &[u8], offset: u64) -> Result<(), RustixErrno> {
+        assert!(buf.len() == PAGE_SIZE as usize);
+        let mut buf = buf;
+        let mut offset = offset;
+        let mut bytes_left = PAGE_SIZE as usize;
+        while bytes_left > 0 {
+            let bytes_written = retry_on_intr(|| pwrite(fd.as_fd(), buf, offset))?;
+            bytes_left -= bytes_written;
+            buf = &buf[bytes_written..];
+            offset += bytes_written as u64;
+        }
+        Ok(())
+    }
+
+    pub fn write_to_disk<Fd: AsFd>(&mut self, fd: Fd) -> Result<(), PageError> {
+        let offset = self.header.page_id * PAGE_SIZE as u64;
+        // setting dirty flag before slice cast and write to:
+        // 1: Make the effects on other vars easier to reason about.
+        // 2: By definition the page on disk should be considered clean
+        let dirty_val = self.header.flags.is_dirty();
+        self.header.flags.set_dirty(false);
+        self.header.checksum = self.calc_checksum();
+        let buf = self.as_slice();
+        Page::write_entire_page(fd, buf, offset).inspect_err(|_err| {
+            self.header.flags.set_dirty(dirty_val);
+        })?;
+        Ok(())
     }
 
     pub fn insert_cell(
@@ -326,7 +401,8 @@ fn checksum(data: &[u8]) -> Result<u64, SerdeError> {
     assert!(data.len() % 8 == 0);
     let mut reader = data;
     let mut sum = 0;
-    for _ in 0..(data.len() / 8) {
+    let chunks = data.len() / 8;
+    for _ in 0..chunks {
         let v = u64::from_bytes(&mut reader, &())?;
         sum += v;
     }
@@ -359,7 +435,9 @@ impl Deserialize for CellPointer {
 
 #[cfg(test)]
 mod tests {
-    use std::mem;
+    use std::{fs::File, mem, path::Path};
+
+    use rustix::fs::{Mode, OFlags};
 
     use super::*;
 
@@ -369,9 +447,9 @@ mod tests {
         assert_eq!(mem::size_of::<PageKind>(), 1);
         assert_eq!(mem::size_of::<Option<PageId>>(), 16);
         assert_eq!(mem::size_of::<Option<u16>>(), 4);
-        assert_eq!(mem::size_of::<PageHeader>(), 48);
+        assert_eq!(mem::size_of::<PageHeader>(), 40);
         assert_eq!(mem::size_of::<PageBuffer>(), PAGE_BUFFER_SIZE as usize);
-        assert_eq!(mem::size_of::<Page>(), PAGE_DATA_SIZE as usize);
+        assert_eq!(mem::size_of::<Page>(), PAGE_SIZE as usize);
         assert_eq!(PAGE_BUFFER_SIZE % 8, 0);
         assert_eq!(mem::size_of::<CellPointer>(), 4);
     }
@@ -550,5 +628,36 @@ mod tests {
         assert_eq!(cell_count, page.header.cell_count);
         assert!(page.header.flags.is_dirty());
         assert!(!page.header.flags.is_compactible());
+    }
+
+    #[test]
+    fn to_from_disk_basics() {
+        let fd = rustix::fs::open(
+            Path::new("to_from_disk_basics.test"),
+            OFlags::CREATE | OFlags::TRUNC | OFlags::RDWR,
+            Mode::RWXU,
+        )
+        .unwrap();
+
+        // add cells
+        let mut page = Page::new(0, PageKind::Data);
+        let cells = vec![
+            vec![1u32, 2, 3, 4, 5],
+            vec![10u32, 20, 30, 40, 50],
+            vec![100u32, 200, 300, 400, 500],
+        ];
+        let mut buffer = Vec::new();
+        let mut data_sizes = Vec::new();
+        for (idx, cell) in cells.iter().enumerate() {
+            cell.write_to_bytes(&mut buffer).unwrap();
+            page.insert_cell(idx as u16, &buffer[..]).unwrap();
+            data_sizes.push(buffer.len());
+            buffer.clear();
+        }
+
+        page.write_to_disk(fd.as_fd()).unwrap();
+
+        let read_page = Page::from_disk(fd.as_fd(), 0).unwrap();
+        assert_eq!(page, read_page);
     }
 }
