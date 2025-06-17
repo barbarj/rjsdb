06725fc844e7b8c417ac4740e01a5e879db77299
rjhallsted <rjhallsted@gmail.com>
2025-06-17 11:10:05 -0600

    Add ability to get serialized size without serialization

--- serialize/src/lib.rs
+++ serialize/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod de;
 pub mod error;
 pub mod ser;
+pub mod serialized_size;
 
 pub use de::{from_reader, Deserializer};
 pub use error::{Error, Result};
 pub use ser::{to_bytes, to_writer, Serializer};
+pub use serialized_size::serialized_size;
 
 #[cfg(not(target_pointer_width = "64"))]
 compile_error!("This serialization format is only supported on 64-bit systems");
@@ -24,14 +26,13 @@ mod tests {
 
     use serde::{Deserialize, Serialize};
 
-    use crate::{from_reader, to_bytes};
+    use crate::{from_reader, serialized_size, to_bytes};
 
     fn assert_value_serdes_correctly<'a, T>(input: T)
     where
         T: Serialize + Deserialize<'a> + PartialEq + Debug,
     {
         let bytes = to_bytes(&input).unwrap();
-        println!("bytes: {bytes:?}");
         let output: T = from_reader(&bytes[..]).unwrap();
         assert_eq!(input, output);
     }
@@ -46,6 +47,15 @@ mod tests {
         let bytes = to_bytes(&input).unwrap();
         let output: Vec<u8> = from_reader(&bytes[..]).unwrap();
         assert_eq!(input, &output);
+    }
+
+    fn assert_value_serialized_size_is_correct<T>(input: &T)
+    where
+        T: Serialize,
+    {
+        let bytes = to_bytes(input).unwrap();
+        let size = serialized_size(input).unwrap();
+        assert_eq!(bytes.len(), size);
     }
 
     #[test]
@@ -76,6 +86,34 @@ mod tests {
         assert_byte_slice_serdes_correctly(byte_slice);
     }
 
+    #[test]
+    fn basic_types_sizes() {
+        // unsigned
+        assert_value_serialized_size_is_correct(&42u8);
+        assert_value_serialized_size_is_correct(&42u16);
+        assert_value_serialized_size_is_correct(&42u32);
+        assert_value_serialized_size_is_correct(&42u64);
+        assert_value_serialized_size_is_correct(&42u128);
+        assert_value_serialized_size_is_correct(&42usize);
+        // signed
+        assert_value_serialized_size_is_correct(&42i8);
+        assert_value_serialized_size_is_correct(&42i16);
+        assert_value_serialized_size_is_correct(&42i32);
+        assert_value_serialized_size_is_correct(&42i64);
+        assert_value_serialized_size_is_correct(&42i128);
+        // floats
+        assert_value_serialized_size_is_correct(&42.42f32);
+        assert_value_serialized_size_is_correct(&42.42f64);
+        // others
+        assert_value_serialized_size_is_correct(&String::from("foobar"));
+        assert_value_serialized_size_is_correct(&"foobar");
+        assert_value_serialized_size_is_correct(&'f');
+        assert_value_serialized_size_is_correct(&vec![31, 32, 33]);
+        assert_value_serialized_size_is_correct(&[31u8, 32u8, 33u8]);
+        let byte_slice = "foooo".as_bytes();
+        assert_value_serialized_size_is_correct(&byte_slice);
+    }
+
     #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
     struct BasicStruct {
         a: usize,
@@ -85,6 +123,11 @@ mod tests {
     #[test]
     fn basic_struct() {
         assert_value_serdes_correctly(BasicStruct { a: 1382, b: 12329 });
+    }
+
+    #[test]
+    fn basic_struct_size() {
+        assert_value_serialized_size_is_correct(&BasicStruct { a: 1382, b: 12329 });
     }
 
     #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
@@ -99,6 +142,13 @@ mod tests {
         assert_value_serdes_correctly(UnitEnum::Foo);
         assert_value_serdes_correctly(UnitEnum::Bar);
         assert_value_serdes_correctly(UnitEnum::Baz);
+    }
+
+    #[test]
+    fn unit_enum_sized() {
+        assert_value_serialized_size_is_correct(&UnitEnum::Foo);
+        assert_value_serialized_size_is_correct(&UnitEnum::Bar);
+        assert_value_serialized_size_is_correct(&UnitEnum::Baz);
     }
 
     #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
@@ -124,9 +174,30 @@ mod tests {
     }
 
     #[test]
+    fn nested_types_size() {
+        let input = NestedTypes {
+            a: 1230,
+            b: -1239,
+            c: UnitEnum::Bar,
+            d: BasicStruct {
+                a: 41231415,
+                b: 1231,
+            },
+        };
+        assert_value_serialized_size_is_correct(&input);
+    }
+
+    #[test]
     fn option() {
         assert_value_serdes_correctly::<Option<u32>>(None);
         assert_value_serdes_correctly(Some(421));
         assert_value_serdes_correctly(Some(UnitEnum::Baz));
+    }
+
+    #[test]
+    fn option_sizes() {
+        assert_value_serialized_size_is_correct::<Option<u32>>(&None);
+        assert_value_serialized_size_is_correct(&Some(421));
+        assert_value_serialized_size_is_correct(&Some(UnitEnum::Baz));
     }
 }
---
+++ serialize/src/serialized_size.rs
@@ -0,0 +1,331 @@
+use serde::{ser, Serialize};
+
+use crate::error::{Error, Result};
+
+pub struct SerializedSize {
+    size: usize,
+}
+impl SerializedSize {
+    fn new() -> Self {
+        SerializedSize { size: 0 }
+    }
+}
+
+pub fn serialized_size<T: Serialize>(value: &T) -> Result<usize> {
+    let mut ser = SerializedSize::new();
+    value.serialize(&mut ser)?;
+    Ok(ser.size)
+}
+
+impl<'a> ser::Serializer for &'a mut SerializedSize {
+    type Ok = ();
+    type Error = Error;
+
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+    type SerializeMap = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
+
+    fn serialize_bool(self, _v: bool) -> Result<()> {
+        self.size += 1;
+        Ok(())
+    }
+
+    fn serialize_i8(self, _v: i8) -> Result<()> {
+        self.size += 1;
+        Ok(())
+    }
+
+    fn serialize_i16(self, _v: i16) -> Result<()> {
+        self.size += 2;
+        Ok(())
+    }
+
+    fn serialize_i32(self, _v: i32) -> Result<()> {
+        self.size += 4;
+        Ok(())
+    }
+
+    fn serialize_i64(self, _v: i64) -> Result<()> {
+        self.size += 8;
+        Ok(())
+    }
+
+    fn serialize_i128(self, _v: i128) -> Result<()> {
+        self.size += 16;
+        Ok(())
+    }
+
+    fn serialize_u8(self, _v: u8) -> Result<()> {
+        self.size += 1;
+        Ok(())
+    }
+
+    fn serialize_u16(self, _v: u16) -> Result<()> {
+        self.size += 2;
+        Ok(())
+    }
+
+    fn serialize_u32(self, _v: u32) -> Result<()> {
+        self.size += 4;
+        Ok(())
+    }
+
+    fn serialize_u64(self, _v: u64) -> Result<()> {
+        self.size += 8;
+        Ok(())
+    }
+
+    fn serialize_u128(self, _v: u128) -> Result<()> {
+        self.size += 16;
+        Ok(())
+    }
+
+    fn serialize_f32(self, v: f32) -> Result<()> {
+        self.size += 4;
+        Ok(())
+    }
+
+    fn serialize_f64(self, v: f64) -> Result<()> {
+        self.size += 8;
+        Ok(())
+    }
+
+    fn serialize_char(self, v: char) -> Result<()> {
+        self.serialize_u32(v.into())
+    }
+
+    fn serialize_str(self, v: &str) -> Result<()> {
+        self.serialize_bytes(v.as_bytes())
+    }
+
+    fn serialize_bytes(self, v: &[u8]) -> Result<()> {
+        self.size += 8 + v.len();
+        Ok(())
+    }
+
+    fn serialize_none(self) -> Result<()> {
+        self.size += 1;
+        Ok(())
+    }
+
+    fn serialize_some<T>(self, value: &T) -> Result<Self::Ok>
+    where
+        T: ?Sized + Serialize,
+    {
+        self.size += 1;
+        value.serialize(self)
+    }
+
+    /// This is a no-op for now. That might be the wrong choice and later I might need
+    /// to come encode something for this
+    fn serialize_unit(self) -> Result<()> {
+        Ok(())
+    }
+
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<()> {
+        self.serialize_unit()
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+    ) -> Result<()> {
+        self.serialize_u32(variant_index)
+    }
+
+    fn serialize_newtype_struct<T>(self, _name: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(self)
+    }
+
+    fn serialize_newtype_variant<T>(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+        value: &T,
+    ) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        self.serialize_u32(variant_index)?;
+        value.serialize(self)
+    }
+
+    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq> {
+        let len = len.expect("Only sequences with known lengths are supported");
+        self.serialize_u64(len as u64)?;
+        Ok(self)
+    }
+
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple> {
+        Ok(self)
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeTupleStruct> {
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+        len: usize,
+    ) -> Result<Self::SerializeTupleVariant> {
+        self.serialize_u32(variant_index)?;
+        self.serialize_seq(Some(len))
+    }
+
+    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap> {
+        let len = len.expect("Only maps with known lengths are supported");
+        self.serialize_u64(len as u64)?;
+        Ok(self)
+    }
+
+    fn serialize_struct(self, _name: &'static str, _len: usize) -> Result<Self::SerializeStruct> {
+        Ok(self)
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant> {
+        self.serialize_u32(variant_index)?;
+        Ok(self)
+    }
+}
+
+impl<'a> ser::SerializeSeq for &'a mut SerializedSize {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> ser::SerializeTuple for &'a mut SerializedSize {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> ser::SerializeTupleStruct for &'a mut SerializedSize {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> ser::SerializeTupleVariant for &'a mut SerializedSize {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> ser::SerializeMap for &'a mut SerializedSize {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_key<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn serialize_value<T>(&mut self, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> ser::SerializeStruct for &'a mut SerializedSize {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _key: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
+
+impl<'a> ser::SerializeStructVariant for &'a mut SerializedSize {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _key: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        Ok(())
+    }
+}
